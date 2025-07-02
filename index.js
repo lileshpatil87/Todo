d3335572e2ba7824f86787eb6403006a8e51d72c
lileshpatil87 <lileshpatil38@gmail.com>
2025-07-02 16:17:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ require("dotenv").config();
 const MONGO = process.env.MONGOURL;
 const PORT = process.env.PORT || 8080;
 
-app.use(cors("*"));
+const allowedOrigins = ["https://todofrontend-10dy.onrender.com"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
